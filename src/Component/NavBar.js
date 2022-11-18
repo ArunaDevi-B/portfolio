639e803962f52e0cad524144042c88673a2fb087
ArunaDevi-B <b.arunadevi30@gmail.com>
2022-11-18 15:42:58 +0530

    drive link added

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,26 +1,11 @@
-import {React, useState } from 'react'
+import {React} from 'react'
 import "../Assets/Css/Navbar.css"
 import Avatar from '@mui/material/Avatar';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Button from '@mui/material/Button';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import Image from "../Assets/Image/profile-pic.jpg"
 
 
 const NavBar = () => {
-//   const [themeMode, setThemeMode] = useState("dark");
-
-// const theme = createTheme({
-//   palette: {
-//     mode: themeMode,
-//   },
-// });
-  
   return (
-    // <ThemeProvider theme={theme}>
-    //   <CssBaseline />
     <div className="nav-bars">
        <div className="nav-logo">
        <Avatar alt="Cindy Baker" src={Image} />
@@ -32,17 +17,13 @@ const NavBar = () => {
                <li onClick={()=>{window.location.replace("/About")}}>About</li>
                <li onClick={()=>{window.location.replace("/Skills")}}>Skills</li>
                <li onClick = {()=>{window.location.replace("/Projects")}}>Project</li>
-               <li onClick = {()=>{window.location.replace("/Contact")}}>Contact</li>
-               {/* <Button className="theme-btn"
-        color="inherit"
-        startIcon = {themeMode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-        onClick={()=> setThemeMode(themeMode == "dark" ? "light" : "dark")} >{themeMode =="dark" ? "Light" : "Dark"} Mode</Button> */}
- 
-
+               <li> <a 
+               className='resume'
+                href='https://drive.google.com/file/d/1_RkuFCoLqvQjnyYgvBoyMy8LLu3BDwJ4/view?usp=sharing' target="_blank" rel="noopener noreferrer">Resume </a></li>
+               
            </ul>
        </div>
     </div>
-    // </ThemeProvider>
 
   )
 }
